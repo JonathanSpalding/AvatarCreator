dc3e72b4521592cf0318d5019fe9d79cc0b49351
JonathanSpalding <Jon.Spalding@hotmail.com>
2018-02-06 22:24:31 -0700

    first commit

---
+++ scripts/controller.js
@@ -0,0 +1,16 @@
+
+// Get and set value of text box using the DOM.
+/*
+function saveHandler() {
+    var editForm = document.forms.editForm;
+
+    // Get the DOM object for the text box.
+    var nameEdit = editForm.name;
+
+    // Get the text in the text box.
+    var text = editForm.value;
+
+    // Set the text box back to blank.
+    nameEdit.value = "";
+}
+*/

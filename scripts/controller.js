ffd54dd10dc716789f844900c1d57b2f236b50df
JonathanSpalding <Jon.Spalding@hotmail.com>
2018-02-08 23:11:32 -0700

    Got it working.

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,9 +1,9 @@
 function onPageLoad() {
     // Wire up all button handlers
-    document.getElementById('createBtn').onclick = onCreateBtnClicked;
-    document.getElementById('cancelBtn').onclick = onCancelBtnClicked;
-    document.getElementById('newBtn').onclick = onNewBtnClicked;
-    document.getElementById("avatar").src = "../img/sprites1.jpg";
+    document.getElementById("createBtn").onclick = onCreateBtnClicked;
+    document.getElementById("cancelBtn").onclick = onCancelBtnClicked;
+    document.getElementById("newBtn").onclick = onNewBtnClicked;
+    document.getElementById("avatar").src = "../img/spritesMaleWhite.jpg";
 }
 
 function onCreateBtnClicked(){
@@ -11,30 +11,26 @@ function onCreateBtnClicked(){
     if (!validateControls()) {
         return;
     }
-
     // Get the data from the form controls.
     var form = document.forms["editForm"];
-
-    // Add the image of the Avatar to the view.
-
-    // Clear the input form and all errors.
-    clearInputForm()
+    // Clear the input form and all errors, then show Avatar.
+    showAvatar();
 }
 
 function onNewBtnClicked() {
     document.getElementById("formTitle").innerHTML = "Customize Avatar";
-
     // Show the form and hide the display area.
+    document.getElementById("avatar").style.display = "block";
     document.getElementById("avatarCreationArea").style.display = "block";
     document.getElementById("avatarDisplayArea").style.display = "none";
-    document.getElementById("createBtn").style.display = "block";
+    document.getElementById("createBtn").style.display = "inline";
 }
 
 function onCancelBtnClicked() {
-    clearInputForm();
+    clearInput();
 }
 
-//////////// Business Logic
+//////////// Business Logic ////////////
 
 function validateControls() {
     var form = document.forms["editForm"];
@@ -42,7 +38,7 @@ function validateControls() {
 
     // name textbox
     if (form.nameEdit.value === "") {
-        document.getElementById("nameError").style.display = "block";
+        document.getElementById("nameError").style.display = "inline";
         document.getElementById("nameError").innerHTML = "Please enter a name!";
         validated = false;
     } else {
@@ -50,14 +46,14 @@ function validateControls() {
     }
     // age textbox
     if (form.ageEdit.value === "") {
-        document.getElementById("ageError").style.display = "block";
+        document.getElementById("ageError").style.display = "inline";
         document.getElementById("ageError").innerHTML = "Please enter an age!";
         validated = false;
     } else if (isNaN(parseInt(form.ageEdit.value))) {
-        document.getElementById("ageError").style.display = "block";
+        document.getElementById("ageError").style.display = "inline";
         document.getElementById("ageError").innerHTML = "Age must be a number.";
     } else if (parseInt(form.ageEdit.value) < 0 || parseInt(form.ageEdit.value) > 999) {
-        document.getElementById("ageError").style.display = "block";
+        document.getElementById("ageError").style.display = "inline";
         document.getElementById("ageError").innerHTML = "Age must be a positive number.";
         validated = false;
     } else {
@@ -65,15 +61,15 @@ function validateControls() {
     }
     // Gender radio buttons
     if (form.genderMaleRadio.checked == false && form.genderFemaleRadio.checked == false) {
-        document.getElementById("genderError").style.display = "block";
-        document.getElementById("genderError").innerHTML = "Please specify a genter!";
+        document.getElementById("genderError").style.display = "inline";
+        document.getElementById("genderError").innerHTML = "Please specify a gender!";
         validated = false;
     } else {
         document.getElementById("genderError").innerHTML = "";
     }
     // Skin Color Dropdown
-    if (form.skinColor.selectedIndex = -1) {
-        document.getElementById("skinColorError").style.display = "block";
+    if (form.skinColor.selectedIndex == -1) {
+        document.getElementById("skinColorError").style.display = "inline";
         document.getElementById("skinColorError").innerHTML = "Please choose a skin color";
         validated = false;
     } else {
@@ -81,20 +77,24 @@ function validateControls() {
     }
     // Sword Check Box. 
     if (form.sword.checked == false) {
-        document.getElementById("swordError").style.display = "block";
+        document.getElementById("swordError").style.display = "inline";
         document.getElementById("swordError").innerHTML = "IT'S DANGEROUS TO GO ALONE! TAKE THIS!";
         validated = false;
     } else {
         document.getElementById("swordError").innerHTML = "";
     }
-
     return validated;
-    
-    // If else statement to see what avatar.
-    if (form.genderMaleRadio.checked == true) {
-        document.getElementById("avatar").src = "../img/sprites1.jpg";
-    } else if (form.genderFemaleRadio.checked == true) {
-        document.getElementById("avatar").src = "../img/sprites2.jpg";
-    }
 }
 
+function clearInput() {
+    document.getElementById("avatarCreationArea").style.display="none";
+    document.getElementById("avatarDisplayArea").style.display="block";
+    document.getElementById("avatar").style.display = "block"; //This line should be removed once the create Avatar function is fully functional.
+}
+function showAvatar() {
+    //var form = document.forms["editForm"];
+    document.getElementById("avatarCreationArea").style.display="none";
+    document.getElementById("avatarDisplayArea").style.display="block";
+    document.getElementById("avatar").style.display="block";
+    GetItemByID();
+}
--- scripts/model.js
+++ scripts/model.js
@@ -1,6 +1,12 @@
 // takes a series of pre-validated parameters, creates a new object, adds the object to the internal list, and returns the new object.
 function CreateItem() {
+    var maleWhite = document.getElementById("avatar").src = "../img/spritesMaleWhite.jpg";
+    var maleBrown = document.getElementById("avatar").src = "../img/spritesMaleBrown.png";
+    var maleBlack = document.getElementById("avatar").src = "../img/spritesMaleBlack.png";
 
+    var femaleWhite = document.getElementById("avatar").src = "../img/spritesFemaleWhite.png";
+    var femaleBrown = document.getElementById("avatar").src = "../img/spritesFemaleBrown.png";
+    var femaleBlack = document.getElementById("avatar").src = "../img/spritesFemaleBlack.png";
 }
 // returns an array of all the items in the list.
 function GetAllItems() {
@@ -8,5 +14,19 @@ function GetAllItems() {
 }
 // takes a series of pre-validated parameters, creates a new object, adds the object to the internal list, and returns the new object.
 function GetItemByID() {
-
+    var form = document.forms["editForm"];
+    // If else statement to see what avatar to use.
+    if (document.getElementById("genderMaleRadio").checked && form.skinColor.selectedIndex == 0) {
+        document.getElementById("avatar").src = "../img/spritesMaleWhite.jpg";
+    } else if (document.getElementById("genderMaleRadio").checked && form.skinColor.selectedIndex == 1) {
+        document.getElementById("avatar").src = "../img/spritesMaleBrown.png";
+    } else if (document.getElementById("genderMaleRadio").checked && form.skinColor.selectedIndex == 2) {
+        document.getElementById("avatar").src = "../img/spritesMaleBlack.png";
+    } else if (document.getElementById("genderFemaleRadio").checked && form.skinColor.selectedIndex == 0) {
+        document.getElementById("avatar").src = "../img/spritesFemaleWhite.png";
+    } else if (document.getElementById("genderFemaleRadio").checked && form.skinColor.selectedIndex == 1) {
+        document.getElementById("avatar").src = "../img/spritesFemaleBrown.png";
+    } else if (document.getElementById("genderFemaleRadio").checked && form.skinColor.selectedIndex == 2) {
+        document.getElementById("avatar").src = "../img/spritesFemaleBlack.png";
+    }
 }
